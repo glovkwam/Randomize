a39302ef0f33eba3bf571b1a14ae1ee99f25e2d5
glovkwam <112836388+glovkwam@users.noreply.github.com>
2022-11-08 17:21:57 -0800

    kwam

--- Randomine2/ran.js
+++ Randomine2/ran.js
@@ -23,66 +23,57 @@ let dogs = [{
 let randomIndex;
 let animating = true;
 let logos = [];
+let imageCounter = 0;
 function preload(){
   
-  for (let i = 0; i <=6; i++){
-    logos[i] = loadImage (`newassets/logo${i}.JPG`)
+  for (let i = 0; i <= 7; i++){
+    logos[i] = loadImage (`logo${i}.JPG`)
   }
 }
 function setup() {
    createCanvas(600,600);
   background(200, 50, 7);
   textSize(35);
-
+  imageMode(CENTER);
+  frameRate(4);
   text("Potential 3x Crypto Projects", 50, 50);
   console.log(logos);
 }
 function draw() {
   if (animating == true){  
-    Image(logos[0], 0, 0);
+    image(logos[imageCounter], width/2, height/1.5);
+    if (imageCounter < logos.length-1){
+      imageCounter++;
+      console.log(imageCounter);
+    }
+    else{
 
-  }
+   imageCounter = 0;
+    }
   
 }
+  function mousePressed(){
+  animating = true;
+  setTimeout (randomizer, 2000);
+}
+  function changeBG() {
+  let val = random(255);
+  background(val);
+}
+
 function randomizer() {
   animating = false;
    if (dogs[0]){
    background (random(200,225));
    randomIndex = int(random(dogs.length));
-   text(`${dogs[randomIndex].name}'s Ticker Symbol is
+     text(`${dogs[randomIndex].name}'s Ticker Symbol is
     ${dogs[randomIndex].color}`,50,50);
+     image(random(logos), width/4, height/2);
+     
    dogs.splice(randomIndex, 1); 
    } else {
    background(random(200, 225));
    text ("not financial advice", 50, 50);
   }
 }
- 
-function mousePressed(){
-  animting = true;
-  setTimeout (randomizer, 2000);
-  // if dogs[0]{
-  // background (random(200,225));
-  // randomIndex = int(random(dogs.length));
-  // text(dogs[randomIndex].name, 50, 50);
-  // dogs.splice(randomIndex, 1); 
-  // } else {
-  // background(random(200, 225));
-  // text ("nothing left", 50, 50);
-  //}
-}
-function medTri(){
-  push();
-  translate(width / 2, height / 2);
-  rotate(count);
-  translate(width * -0.5, height * -0.5);
-  fill(random(225),random(225),random(225));
-  beginShape();
-  vertex(width * 0.2, height * 0.4);
-  vertex(width * 0.3, height * 0.3);
-  vertex(width * 0.7, height * 0.9);
-  vertex(width * 0.8, height * 0.2);
-  vertex(width * 0.4, height * 0.6);
-  endShape(CLOSE);
-  pop();
 }
--- sketch.js
+++ sketch.js
@@ -1,47 +1,89 @@
 let dogs = [{
   name: "Api3",
-  color: "Blockchain1"
+  color: "$Api3"
 }, {
   name: "Quant",
-  color: "Blockchain2"
+  color: "$QNT"
 }, {
-  name: "RSR",
-  color: "Blockchain3"  
+  name: "Shina Inu",
+  color: "$shib"  
 },{ 
-  name: "CRO",
-  color: "Blockhain4" 
+  name: "Crypto.com",
+  color: "$CRO" 
 },{
-  name: "Jasmy",
-  color: "Blockhain5"
+  name: "JasmyCoin",
+  color: "$Jasmy"
 },{
   name:"Kadena",
-  color: "Blockchain6"
+  color: "$KDA"
 },{
   name:"Saitama",
-  color:"Blockchain7"
+  color:"$Saitama"
 }];
 let randomIndex;
+let animating = false;
+let logos = [];
+let imageCounter = 0;
+let button;
 
+function preload(){
+  
+  for (let i = 0; i <= 7; i++){
+    logos[i] = loadImage (`logo${i}.JPG`)
+  }
+}
 function setup() {
    createCanvas(600,600);
-  background(200);
+  background(200, 50, 7);
   textSize(35);
-  text("click to Randomize", 50, 50);
-
-}
-function draw() {
+  textFont('Courier new');
+ // textAllign(CENTER);
+  textStyle(BOLD);
+  imageMode(CENTER);
+  frameRate(4);
+  
+  text("Potential 3x Crypto Projects", 50, 50);
+  console.log(logos);
+  fill(random(255), random(255),random(255));
+  
+  button = createButton("Click");
+  button.mousePressed(buttonPressed);
   
 }
-function mousePressed(){
-
-if (dogs [0]){
-background (random(200, 255));
-randomIndex = int( random(dogs.length));
-text(dogs [randomIndex].name, 50, 50);
-dogs.splice(randomIndex, 1);
-} else {
-  background (random(200, 255));
-
-  text("Subscribe to CryptoKwam", 50, 50);
+function draw() {
+  if (animating == true){  
+    clear();
+    image(logos[imageCounter], width/2, height/1.5);
+    if (imageCounter < logos.length - 1){
+      imageCounter++;
+      console.log(imageCounter);
+    } else {
+    imageCounter = 0;
+    }
+  
 }
+ 
+function randomizer() {
+  animating = false;
+   if (dogs[0]){
+     
+   background (rando(255), random(255), random(255));
+   randomIndex = int(random(dogs.length));
+  // text(`${dogs[randomIndex].name}'s Ticker Symbol is
+    //${dogs[randomIndex].color}`,50,50);
+     image(random(logos), windowWidth/2, windowHeight/2);
+      text(`${dogs[randomIndex].name}'s Ticker Symbol is
+    ${dogs[randomIndex].color}`,50,50);
+     fill(random(255),random(255), random(255));
+   dogs.splice(randomIndex, 1); 
+   } else {
+   background(random(200, 225));
+   text ("not financial advice", 50, 50);
+  }
+}
+
+}
+function buttonPressed(){
+  animating = true;
+  setTimeout(randomizer,1900);
 }
